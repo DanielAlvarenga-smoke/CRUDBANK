e51124f01476fc755b79f227b92a26fa82667659
DanielAlvarenga-smoke <red57sw2@gmail.com>
2022-12-05 21:42:40 -0300

    entraga crud bank

---
+++ Trabalho Final - Lab. Poo/src/jFrames/main.java
@@ -0,0 +1,15 @@
+package jFrames;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        TelaLogin tela = new TelaLogin();
+        tela.setLocationRelativeTo(null);
+        tela.setResizable(false);
+        tela.setVisible(true); 
+                       
+//       O Usuário é: pedrodaniel
+//       A Senha é: 2022201796020222018500
+    } 
+}
